533a21ffe67abef89abc276cc7fe365d01f8e75f
H.J. Lu <hjl.tools@gmail.com>
2016-03-05 07:17:09 -0800

    Update i386 piecewise move and store
    We can use TImode/OImode/XImode integers for piecewise move and store.
    When vector register is used for piecewise move and store, we don't
    increase stack_alignment_needed since vector register spill isn't
    required for piecewise move and store.  Since stack_realign_needed is
    set to true by checking stack_alignment_estimated set by pseudo vector
    register usage, we also need to check stack_realign_needed to eliminate
    frame pointer.
    
    	* config/i386/i386.c (ix86_finalize_stack_realign_flags): Also
    	check stack_realign_needed for stack realignment.
    	(ix86_legitimate_constant_p): Always allow CONST_WIDE_INT smaller
    	than the largest integer supported by vector register.
    	* config/i386/i386.h (MOVE_MAX): Set to 64.
    	(MOVE_MAX_PIECES): Set to bytes of the largest integer supported
    	by vector register.
    	(STORE_MAX_PIECES): New.

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -12820,8 +12820,15 @@ ix86_finalize_stack_realign_flags (void)
   /* If the only reason for frame_pointer_needed is that we conservatively
      assumed stack realignment might be needed, but in the end nothing that
      needed the stack alignment had been spilled, clear frame_pointer_needed
-     and say we don't need stack realignment.  */
-  if (stack_realign
+     and say we don't need stack realignment.
+
+     When vector register is used for piecewise move and store, we don't
+     increase stack_alignment_needed as there is no register spill for
+     piecewise move and store.  Since stack_realign_needed is set to true
+     by checking stack_alignment_estimated which is updated by pseudo
+     vector register usage, we also need to check stack_realign_needed to
+     eliminate frame pointer.  */
+  if ((stack_realign || crtl->stack_realign_needed)
       && frame_pointer_needed
       && crtl->is_leaf
       && flag_omit_frame_pointer
@@ -14652,7 +14659,7 @@ darwin_local_data_pic (rtx disp)
    satisfies CONSTANT_P.  */
 
 static bool
-ix86_legitimate_constant_p (machine_mode, rtx x)
+ix86_legitimate_constant_p (machine_mode mode, rtx x)
 {
   /* Pointer bounds constants are not valid.  */
   if (POINTER_BOUNDS_MODE_P (GET_MODE (x)))
@@ -14719,7 +14726,14 @@ ix86_legitimate_constant_p (machine_mode, rtx x)
       break;
 
     case CONST_WIDE_INT:
-      if (!TARGET_64BIT && !standard_sse_constant_p (x))
+      if (!TARGET_64BIT
+	  && !standard_sse_constant_p (x)
+	  && GET_MODE_SIZE (TARGET_AVX512F
+			    ? XImode
+			    : (TARGET_AVX
+			       ? OImode
+			       : (TARGET_SSE2
+				  ? TImode : DImode))) < GET_MODE_SIZE (mode))
 	return false;
       break;
 
