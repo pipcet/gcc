bb982bc4991673bbe4f2f11040226813fa9c15ec
Pip Cet <pipcet@gmail.com>
2016-11-24 05:14:03 +0000

    wasm32 support, WIP

---
+++ gcc/config/wasm32/wasm32-import-macros.s
@@ -0,0 +1,41 @@
+#NO_APP
+        .macro .import3 smodule sextname name
+        .pushsection .str.lib,"ams",1
+        .data
+.ifndef __str_\name
+__str_\name:
+        .ascii "\sextname"
+        .byte 0
+.endif
+.ifndef __str_\smodule
+__str_\smodule:
+        .ascii "\smodule"
+        .byte 0
+.endif
+        .text
+        .global \name
+	defun \name, FiiiiiiiE
+	i32.const -16
+	get_local $sp1
+	i32.add
+	set_local $sp
+	get_local $sp
+	set_local $fp
+        i32.const __str_\smodule
+	set_local $r0
+        i32.const __str_\name
+	set_local $r1
+        get_local $pc0
+        get_local $dpc
+        get_local $sp
+        i32.const 1
+        i32.const 2
+        i32.const 3
+        call $syscall
+        set_local $r0
+        get_local $fp
+        return[1]
+        end
+	endefun \name
+        .popsection
+        .endm
