02d59039489297c2a1b70e2ed1ab82a7d88d5912
Pip Cet <pipcet@gmail.com>
2016-12-04 10:35:15 +0000

    debugging macro

--- gcc/config/wasm32/wasm32-import-macros.s
+++ gcc/config/wasm32/wasm32-import-macros.s
@@ -1,4 +1,19 @@
 #NO_APP
+        .macro T a
+        i32.const \a
+        get_local $sp
+        get_local $fp
+        get_local $rp
+        get_local $dpc
+        .ifdef __wasm_blocks
+        i32.const __wasm_blocks
+        .else
+        i32.const -1
+        .endif
+        call $trace
+        drop
+        .endm
+
         .macro .import3 module extname name
         .pushsection .str.lib,"ams",1
         .data
