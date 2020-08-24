4b9fca91daf0a0c052c57add93c71ed09cb16841
Pip Cet <pipcet@gmail.com>
2020-08-24 16:17:23 +0000

    cmpelim fix

--- gcc/compare-elim.c
+++ gcc/compare-elim.c
@@ -202,7 +202,7 @@ arithmetic_flags_clobber_p (rtx_insn *insn)
   if (asm_noperands (pat) >= 0)
     return false;
 
-  if (GET_CODE (pat) == PARALLEL && XVECLEN (pat, 0) == 2)
+  if (GET_CODE (pat) == PARALLEL && XVECLEN (pat, 0) >= 2)
     {
       x = XVECEXP (pat, 0, 0);
       if (GET_CODE (x) != SET)
@@ -663,6 +663,17 @@ static rtx
 try_validate_parallel (rtx set_a, rtx set_b)
 {
   rtx par = gen_rtx_PARALLEL (VOIDmode, gen_rtvec (2, set_a, set_b));
+  if (GET_CODE (set_b) == PARALLEL)
+    {
+      int len = XVECLEN (set_b, 0);
+      rtvec v = rtvec_alloc (len);
+      RTVEC_ELT (v, 0) = set_a;
+      RTVEC_ELT (v, 1) = XVECEXP (set_b, 0, 0);
+      for (int i = 1; i < len - 1; i++)
+	RTVEC_ELT (v, i + 1) = XVECEXP (set_b, 0, i + 1);
+
+      par = gen_rtx_PARALLEL (VOIDmode, v);
+    }
   rtx_insn *insn = make_insn_raw (par);
 
   if (insn_invalid_p (insn, false))
@@ -873,10 +884,13 @@ try_eliminate_compare (struct comparison *cmp)
      [(set (reg:CCM) (compare:CCM (operation) (immediate)))
       (set (reg) (operation)]  */
 
-  rtvec v = rtvec_alloc (2);
+  int len = XVECLEN (PATTERN (insn), 0);
+  rtvec v = rtvec_alloc (len);
   RTVEC_ELT (v, 0) = y;
   RTVEC_ELT (v, 1) = x;
-  
+  for (int i = 1; i < len - 1; i++)
+    RTVEC_ELT (v, i + 1) = XVECEXP (PATTERN (insn), 0, i + 1);
+
   rtx pat = gen_rtx_PARALLEL (VOIDmode, v);
   
   /* Succeed if the new instruction is valid.  Note that we may have started
