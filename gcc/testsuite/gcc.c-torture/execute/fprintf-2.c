8ad168cf0816e3ec264bd63e604e896fd8031d1c
Pip Cet <pipcet@gmail.com>
2021-01-25 16:56:16 +0000

    gcc/testsuite/gcc.c-torture/execute/fprintf-2.c: skip on wasm32

--- gcc/testsuite/gcc.c-torture/execute/fprintf-2.c
+++ gcc/testsuite/gcc.c-torture/execute/fprintf-2.c
@@ -4,6 +4,7 @@
    { dg-require-effective-target fileio }
    { dg-prune-output "warning: warning: \[^\n\r\]* possibly used unsafely" }
    { dg-skip-if "requires io" { avr-*-* } }
+   { dg-skip-if "requires io" { wasm32-*-* } }
    { dg-skip-if "requires io" { freestanding } } */
 
 #include <stdio.h>
