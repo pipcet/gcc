e5deb474769fd4c671767c6397268f2abc1f2620
Pip Cet <pipcet@gmail.com>
2021-01-27 21:34:10 +0000

    gcc/config/wasm32/wasm32.c: no longer need __wasm32_fallthrough

--- gcc/config/wasm32/wasm32.c
+++ gcc/config/wasm32/wasm32.c
@@ -3294,7 +3294,7 @@ const char *
 wasm32_expand_ret_insn()
 {
   char buf[1024];
-  snprintf (buf, 1022, "i32.const %d\n\tlocal.get $fp\n\ti32.add\n\treturn\n\t.set __wasm32_fallthrough, 0",
+  snprintf (buf, 1022, "i32.const %d\n\tlocal.get $fp\n\ti32.add\n\treturn",
 	    wasm32_function_regsize (NULL_TREE));
 
   return ggc_strdup (buf);
